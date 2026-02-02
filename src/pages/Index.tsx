f2cecc1a4e5ed7bd3686b53aa23049a4da7f6701
TMC <b10603220@gmail.com>
2026-02-02 12:02:11 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -106,7 +106,7 @@ const Index = () => {
 
   const handleResetAll = () => {
     clearAll();      // 清掉所有繪圖資料
-    setCircles([]);
+    setCircle(null);
     setIsCircleSelected(false);
     setImage(null);  // 清掉圖片資料
   };
